42528d8591441bd38ffec777af943dcbea95dc45
chandapwes <67353485+chandapwes@users.noreply.github.com>
2021-07-16 15:47:42 -0600

    wip

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,26 @@
 // build your `Resource` model here
+const db = require('../../data/dbConfig')
+
+module.exports = {
+    get,
+    getById,
+    insert,
+}
+
+function get() {
+    return db('resources')
+}
+
+function getById(id) {
+    return db('resources')
+        .where({ id })
+        .first()
+}
+
+function insert(resource) {
+    return db('resource')
+    .insert(resource)
+    .then(ids => {
+        return getById(ids[0])
+    })
+}
